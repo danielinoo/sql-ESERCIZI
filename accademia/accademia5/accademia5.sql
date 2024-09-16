be6d194c860ec38767646229678425bc506b39a3
danielinoo <daniele.pietropaolo1234@gmail.com>
2024-09-16 13:52:12 +0200

    accademia5 da finire

--- accademia/accademia5/accademia5.sql
+++ accademia/accademia5/accademia5.sql
@@ -6,21 +6,18 @@ where WP.nome = 'Pegasus';
 
 --2
 
-select Persona.nome, Persona.cognome, Persona.posizione
-from Persona 
-where   AttivitaProgetto.progetto = 'Pegasus' 
-        and AttivitaProgetto.persona = Persona.id
+select distinct Persona.nome, Persona.cognome, Persona.posizione
+from Persona ,AttivitaProgetto,progetto,WP
+where  Progetto.nome = 'Pegasus'
+and AttivitaProgetto.progetto = WP.progetto and AttivitaProgetto.wp = WP.id
+and WP.progetto = Progetto.id
+and AttivitaProgetto.persona = Persona.id
 order by Persona.cognome desc;
 
 
 --3
+select pero
 
-select Persona.nome, Persona.cognome, Persona.posizione
-from (  select Persona.id
-        from Persona 
-        where AttivitaProgetto.progetto = 'Pegasus' 
-        and AttivitaProgetto.persona = Persona.id) as pp
-where   pp.persona
 
 --4
 select Persona.nome , Persona.cognome , Persona.posizione
