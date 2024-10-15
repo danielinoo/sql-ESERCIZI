b22e86852bb1e36aff9cf7611acc86536a624c1a
danielinoo <daniele.pietropaolo1234@gmail.com>
2024-10-15 19:54:51 +0200

    ACCADEMIAN7 DA FINIRE

---
+++ accademia/accademia7/accademia7.sql
@@ -0,0 +1,38 @@
+--1 da rivedere
+SELECT posizione, AVG(stipendio) AS media_stipendio, STDDEV(stipendio) AS deviazione_standard_stipendio
+FROM Persona
+GROUP BY posizione;
+
+--2
+
+WITH pp AS (
+    SELECT  AVG(stipendio) AS media_stipendio
+    FROM Persona
+    WHERE Persona.posizione = 'Ricercatore'
+)
+SELECT p.*
+FROM Persona p
+WHERE  p.posizione = 'Ricercatore'
+and p.stipendio > pp.media_stipendio;
+
+--3
+
+--4
+SELECT p* , SUM(ap.oreDurata) as ore_lavoro
+FROM Persona p,AttivitaProgetto ap
+JOIN AttivitaProgetto  ap ON p.id = ap.persona
+GROUP BY p
+HAVING SUM(ap.oreDurata) >= 20;
+
+--5
+
+--6
+SELECT p.nome, SUM(ap.oreDurata) AS ore_tot
+FROM Progetto p, AttivitaProgetto
+JOIN AttivitaProgetto ap ON P.id = ap.progetto
+WHERE p.fine < CURRENT_DATE 
+AND ap.tipo = 'Dimostrazione'
+GROUP BY p.nome;
+
+--7
+
