d788962cd33225f2ae54ecd4f5f2c4ef2a804202
danielinoo <daniele.pietropaolo1234@gmail.com>
2024-10-16 18:25:15 +0200

    accademia7 da finire (vedere 3)

--- accademia/accademia7/accademia7.sql
+++ accademia/accademia7/accademia7.sql
@@ -1,4 +1,4 @@
---1 da rivedere--ok
+--1--ok
 SELECT posizione, AVG(stipendio) AS media_stipendio, STDDEV(stipendio) AS deviazione_standard_stipendio
 FROM Persona
 GROUP BY posizione;
@@ -15,18 +15,34 @@ WHERE  p.posizione = 'Ricercatore'
 and p.stipendio > pp.media_stipendio;
 
 
-
---3
-
---4--rivedere
-SELECT p , SUM(ap.oreDurata) as ore_lavoro
-FROM Persona p,AttivitaProgetto 
-JOIN AttivitaProgetto  ap ON p.id = ap.persona
-GROUP BY p
+--3--da rivedere
+WITH pa as (
+    SELECT posizione, AVG(stipendio) AS media_stipendio, STDDEV(stipendio) AS deviazione_standard_stipendio
+    FROM Persona
+    GROUP BY posizione
+)
+SELECT p.posizione as posizione,count(p.id) as numero
+FROM Persona p
+JOIN pa ON p.posizione = pa.posizione
+WHERE p.stipendio >= (pa.media_stipendio - pa.deviazione_standard_stipendio)
+    and p.stipendio >= (pa.media_stipendio - pa.deviazione_standard_stipendio)
+GROUP BY p.posizione
+--4--ok
+SELECT p.id,p.nome,p.cognome,p.posizione,p.stipendio , SUM(ap.oreDurata) as ore_lavoro
+FROM AttivitaProgetto ap
+JOIN Persona p ON p.id = ap.persona
+GROUP BY p.id,p.nome,p.cognome,p.posizione,p.stipendio 
 HAVING SUM(ap.oreDurata) >= 20;
 
---5
-
+--5--ok
+WITH qq as (
+    SELECT avg(p.fine - p.inizio) as durata_media
+    FROM Progetto p
+)
+SELECT p.nome, (p.fine - p.inizio) as numero
+FROM Progetto p , qq
+WHERE 
+    (p.fine - p.inizio) > qq.durata_media
 
 --6--ok
 SELECT p.nome, SUM(ap.oreDurata) AS ore_tot
@@ -36,4 +52,30 @@ WHERE p.fine < CURRENT_DATE
 AND ap.tipo = 'Dimostrazione'
 GROUP BY p.nome;
 
---7
+--7--ok
+WITH a_pa as(
+    SELECT p.id,p.nome,count(*) as numero_assenze
+    FROM Assenza a, Persona p
+    WHERE a.tipo = 'Malattia'
+        AND  a.persona = p.id
+        AND p.posizione = 'Professore Associato'
+    GROUP BY p.id,p.nome,p.cognome
+),
+a_po as(
+    SELECT p.id as id,p.nome as nome, p.cognome as cognome ,count(*) as numero_assenze
+    FROM Assenza a, Persona p
+    WHERE a.tipo = 'Malattia'
+        AND  a.persona = p.id
+        AND p.posizione = 'Professore Ordinario'
+    GROUP BY p.id,p.nome,p.cognome
+),
+
+n_media_a as (
+    SELECT avg(numero_assenze) as num_media_ga
+    FROM a_pa
+
+)
+
+SELECT a_po.id,a_po.nome ,a_po.cognome
+FROM a_po, n_media_a
+WHERE a_po.numero_assenze > n_media_a.num_media_ga
