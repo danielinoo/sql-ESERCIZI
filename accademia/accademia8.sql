d64e5241a5405421a29e238afa55522369dce815
danielinoo <daniele.pietropaolo1234@gmail.com>
2024-10-29 15:49:35 +0100

    accademia8: rivedere 2,5

--- accademia/accademia8.sql
+++ accademia/accademia8.sql
@@ -1,5 +1,5 @@
 --1--con union
-SELECT p.id
+SELECT p.id,p.nome,p.cognome
 FROM Persona p
 WHERE
 
@@ -22,25 +22,50 @@ AND a.giorno = anp.giorno
 
 
 
-
+----------------------------------------------------------------------
 --2--rivedere perche ci sta anna bianchi
---durata progetto pegasus
-WITH durata_progetto AS (
-    SELECT id,inizio, fine
-    FROM Progetto
-    WHERE nome = 'Pegasus'
+-- WITH dp AS (
+--     SELECT p.inizio AS inizio, p.fine AS fine
+--     FROM Progetto p
+--     WHERE p.nome = 'Pegasus'
+-- )
+-- SELECT p.id, p.nome, p.cognome
+-- FROM Persona p
+-- LEFT JOIN AttivitaProgetto ap ON ap.persona = p.id 
+--    AND (ap.giorno >= (SELECT inizio FROM dp) AND ap.giorno <= (SELECT fine FROM dp))
+-- LEFT JOIN AttivitaNonProgettuale anp ON anp.persona = p.id 
+--    AND (anp.giorno >= (SELECT inizio FROM dp) AND anp.giorno <= (SELECT fine FROM dp))
+-- WHERE ap.id IS NULL OR anp.id IS NULL
+-- ORDER BY p.id asc
+
+
+
+--con except senza left join
+WITH dp AS (
+    SELECT p.inizio AS inizio, p.fine AS fine
+    FROM Progetto p
+    WHERE p.nome = 'Pegasus'
+),
+
+--persone che non c hanno attivita nella durata di pegasus
+per as (
+--seleziono tutte le persone
+select p.id as id
+from persona p
+EXCEPT
+
+--seleziono le persone senza attivita
+select distinct ap.persona
+from AttivitaProgetto ap, Progetto pr,dp
+where pr.nome = 'Pegasus'
+and ap.giorno BETWEEN pr.inizio AND pr.fine
 )
 
-SELECT p.id, p.nome, p.cognome
-FROM Persona p
-LEFT JOIN AttivitaProgetto ap ON p.id = ap.persona
-LEFT JOIN WP wp ON ap.wp = wp.id
-LEFT JOIN durata_progetto dp ON wp.progetto = dp.id
-WHERE (dp.inizio IS NULL AND dp.fine IS NULL)  -- Nessun progetto trovato
-   OR (ap.giorno < dp.inizio OR ap.giorno > dp.fine)  -- Attività Progetto fuori dall'intervallo
-GROUP BY p.id, p.nome, p.cognome
+SELECT p.id,p.nome,p.cognome
+from Persona p,per
+where p.id = per.id
 order by p.id asc
-
+------------------------------------------------------------
 
 --3--okk
 
