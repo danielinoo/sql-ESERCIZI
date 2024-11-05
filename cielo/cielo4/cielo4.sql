b6f5bc4188752039162eb59f7f5c342aa0ec2c7a
danielinoo <daniele.pietropaolo1234@gmail.com>
2024-11-05 12:51:02 +0100

    CIELO4 (finito 1,2,4) RIVEDERE 3,5,6

--- cielo/cielo4/cielo4.sql
+++ cielo/cielo4/cielo4.sql
@@ -48,4 +48,31 @@ WHERE BA.num_volo = AB.num_volo
 GROUP BY ap.partenza , ap.arrivo
 
 
+--4--ok
+WITH durata_compagnia as (
+    SELECT v.comp as nome, avg(v.durataMinuti) as media
+    FROM  Volo v
+    GROUP BY v.comp
+)
+SELECT DISTINCT v.comp
+FROM Volo v, durata_compagnia dc
 
+GROUP BY v.comp,dc.media
+HAVING AVG(v.durataMinuti) > dc.media
+
+--5--rivedere
+SELECT a.codice
+from Volo v
+JOIN ArrPart ap ON  ap.codice = v.codice and ap.comp = v.comp
+JOIN Aeroporto a ON ap.partenza = a.codice 
+JOIN LuogoAeroporto la on a.codice = la.aeroporto
+GROUP BY a.codice
+HAving count(la.aeroporto) >= 2
+
+--6--rivedere
+WITH la1 as 
+(
+    SELECT la.citta as citta , a.codice as codice 
+    FROM LuogoAeroporto la , Aeroporto a
+    where la.aeroporto = a.codice and la.citta <> la.citta
+)
